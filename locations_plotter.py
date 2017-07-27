1c0bbafc6f1cf543a28dec06bb9e75697090828d
Manoj <manojy@utl.in>
2017-07-27 14:35:01 +0530

    Added code to get coordinates of all locations

--- locations_plotter.py
+++ locations_plotter.py
@@ -2,20 +2,22 @@ import geopy
 import pandas
 import openpyxl
 import math
+import time
 from openpyxl import load_workbook
 from geopy.geocoders import Nominatim
 
 print("Plotting locations...")
 
 ###################### GLOBAL VARIABLES ########################################
-all_locations = pandas.read_excel("all_locations.xlsx",sheetname="FROM_TO_COMBINED")
+all_locations = pandas.read_excel("all_locations_temp.xlsx",sheetname="FROM_TO_COMBINED")
+all_locations_metadata = pandas.read_excel("all_locations_temp.xlsx",sheetname="metadata")
 nom = geopy.Nominatim()
 
 ###################### FUNCTION DEFINITIONS ####################################
 
 def update_metadata_to_excel(dataframe, sheetname):
-	book = load_workbook('all_locations.xlsx')
-	writer = pandas.ExcelWriter('all_locations.xlsx', engine='openpyxl')
+	book = load_workbook("all_locations_temp.xlsx")
+	writer = pandas.ExcelWriter("all_locations_temp.xlsx", engine="openpyxl")
 	writer.book = book
 	writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
 	dataframe.to_excel(writer, sheetname)
@@ -25,23 +27,39 @@ def update_metadata_to_excel(dataframe, sheetname):
 
 def update_node_location_in_excel():
 
-	all_locations["SEARCH_LOCATION"] = all_locations["STN NAME"] + ", India"
-	print("plotting location, it may take some time...")
+	all_locations_metadata["SEARCH_LOCATION"] = all_locations["STN NAME"] + ", India"
+	update_metadata_to_excel(all_locations_metadata, "metadata")
 
-	all_locations["RAW_LOCATION"] = all_locations["SEARCH_LOCATION"].apply(nom.geocode)
-	all_locations["LAT"] = all_locations["RAW_LOCATION"].apply(lambda x: x.latitude if x != None else None)
-	all_locations["LONG"] = all_locations["RAW_LOCATION"].apply(lambda x: x.longitude if x != None else None)
+	starting_index = input("Enter starting index: ")
+	ending_index = input("Enter ending index: ")
 
-	update_metadata_to_excel(all_locations, "metadata")
+	for i in range(int(starting_index),int(ending_index)):
+		print("I: "+str(i)+" plotting for " + all_locations_metadata.loc[i,"SEARCH_LOCATION"])
+		raw_location = nom.geocode(all_locations_metadata.loc[i,"SEARCH_LOCATION"])
+		if raw_location != None:
+			all_locations_metadata.loc[i,"LAT"] = raw_location.latitude
+			all_locations_metadata.loc[i,"LONG"] = raw_location.longitude
+		else:
+			all_locations_metadata.loc[i,"LAT"] = "XXX"
+			all_locations_metadata.loc[i,"LONG"] = "XXX"
+		
+		update_metadata_to_excel(all_locations_metadata, "metadata")
+
+#	print("plotting location, it may take some time...")
+
+#	all_locations["RAW_LOCATION"] = all_locations["SEARCH_LOCATION"].apply(nom.geocode)
+#	all_locations["LAT"] = all_locations["RAW_LOCATION"].apply(lambda x: x.latitude if x != None else None)
+#	all_locations["LONG"] = all_locations["RAW_LOCATION"].apply(lambda x: x.longitude if x != None else None)
+
 
 # ------------------------------------------------------------------------------
 
 ###################### MAIN ####################################################
-A = nom.geocode("Bengaluru, India")
-print("Bengaluru location: ")
-print(A)
+#A = nom.geocode("Bengaluru, India")
+#print("Bengaluru location: ")
+#print(A)
 
-print("plotting locations..")
+#print("plotting locations..")
 update_node_location_in_excel()
 
 # TODO
